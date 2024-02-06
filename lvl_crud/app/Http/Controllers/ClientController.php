94cb837208545113a10032d2fb1c52c1131f15bb
vashura <valerio.lopez@outlook.com>
2024-02-06 00:35:29 -0600

    se agrega una vista para que el controlador de clientes muestre una page

--- lvl_crud/app/Http/Controllers/ClientController.php
+++ lvl_crud/app/Http/Controllers/ClientController.php
@@ -12,6 +12,7 @@ class ClientController extends Controller
      */
     public function index()
     {
+        return view('client.index');
     }
 
     /**
---
+++ lvl_crud/resources/views/client/index.blade.php
@@ -0,0 +1,29 @@
+@extends('theme.base')
+
+
+<!-- se define una seccion a reemplazar dentro del archivo que se extiende-->
+@section('content')
+
+    <div class="container py-5 text-center">
+        <h1>
+            Listado de Clientes!
+        </h1>
+        <a href="{{ route('client.index') }}" class="btn btn-primary">Crear Clientes</a>
+
+       <table class="table">
+            <thead>
+                <th>Nombre</th>
+                <th>Saldo</th>
+                <th>Acciones</th>
+            </thead> 
+            <tbody>
+                <tr>
+                    <td>FelinoHost</td>
+                    <td>0.0</td>
+                    <td>editar - eliminar </td>
+                </tr>
+            </tbody>
+       </table>
+    </div>
+    
+@endsection
